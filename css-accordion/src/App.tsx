0ad21a0e4d97a5a821e569acb1c2cc033801cf7f
unknown <florie.anstett@gmail.com>
2022-07-05 16:50:43 +0200

    desktop done

--- css-accordion/src/App.tsx
+++ css-accordion/src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const faq: { id: string; color: string; question: string; answer: string }[] = [
+  {
+    id: "01",
+    color: "turquoise",
+    question: "How do I apply ?",
+    answer:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate maxime vel praesentium at dolor doloremque temporibus impedit, consequatur distinctio necessitatibus reiciendis delectus adipisci nemo, explicabo tenetur eaque aut sequi perferendis.",
+  },
+  {
+    id: "02",
+    color: "blue",
+    question: "Lost or forgotten your password ?",
+    answer:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate maxime vel praesentium at dolor doloremque temporibus impedit, consequatur distinctio necessitatibus reiciendis delectus adipisci nemo, explicabo tenetur eaque aut sequi perferendis.",
+  },
+  {
+    id: "03",
+    color: "purple",
+    question: "How to change my username ?",
+    answer:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate maxime vel praesentium at dolor doloremque temporibus impedit, consequatur distinctio necessitatibus reiciendis delectus adipisci nemo, explicabo tenetur eaque aut sequi perferendis.",
+  },
+  {
+    id: "04",
+    color: "pink",
+    question: "How to delete my account ?",
+    answer:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate maxime vel praesentium at dolor doloremque temporibus impedit, consequatur distinctio necessitatibus reiciendis delectus adipisci nemo, explicabo tenetur eaque aut sequi perferendis.",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Frequently asked questions</h1>
+        {faq.map((tab, i) => {
+          return (
+            <div className="tab" key={i}>
+              <input type="radio" name="faq" id={tab.id} />
+              <label htmlFor={tab.id}>
+                <h2 className={tab.color}>{tab.id}</h2>
+                <h3>{tab.question}</h3>
+              </label>
+              <div className="content">
+                <p>{tab.answer}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
